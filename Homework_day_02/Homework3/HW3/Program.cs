88a50e769f1d7dfa37ac48a4cbf08decf5a3f18c
MateAbulashvili <abulashvilimate@gmail.com>
2021-10-25 09:06:55 -0400

    add homework 2

---
+++ Homework_day_02/Homework3/HW3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number please: ");
+
+            int i = int.Parse((Console.ReadLine());
+
+            double Power = Math.Pow(i, 2);
+
+            Console.WriteLine("power of the number is "+ Power);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework_day_02/Homework5/HW5/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace HW5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the date of birth: ");
+            int date = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the month:");
+            string monthOfTheYear = Console.ReadLine();
+            string someVar = "";
+            switch (monthOfTheYear)
+            {
+                case "January":
+                    if (date < 20)
+                        someVar = "Capricorn";
+                    else
+                        someVar = "Aquarius";
+                    break;
+                case "February":
+                    if (date < 19)
+                        someVar = "Aquarius";
+                    else
+                        someVar = "Pisces";
+                    break;
+                case "March":
+                    if (date < 21)
+                        someVar = "Pisces";
+                    else
+                        someVar = "Aries";
+                    break;
+                case "April":
+                    if (date < 20)
+                        someVar = "Aries";
+                    else
+                        someVar = "Taurus";
+                    break;
+                case "May":
+                    if (date < 21)
+                        someVar = "Taurus";
+                    else
+                        someVar = "Gemini";
+                    break;
+                case "June":
+                    if (date < 21)
+                        someVar = "Gemini";
+                    else
+                        someVar = "Cancer";
+                    break;
+                case "July":
+                    if (date < 23)
+                        someVar = "Cancer";
+                    else
+                        someVar = "Leo";
+                    break;
+                case "August":
+                    if (date < 23)
+                        someVar = "Leo";
+                    else
+                        someVar = "Virgo";
+                    break;
+                case "September":
+                    if (date < 23)
+                        someVar = "Virgo";
+                    else
+                        someVar = "Libra";
+                    break;
+                case "October":
+                    if (date < 23)
+                        someVar = "Libra";
+                    else
+                        someVar = "Scorpio";
+                    break;
+                case "November":
+                    if (date < 22)
+                        someVar = "Scorpio";
+                    else
+                        someVar = "Sagittarius";
+                    break;
+                default :
+                    if (date < 22)
+                        someVar = "Sagittarius";
+                    else
+                        someVar = "apricorn";
+                    break;
+            }
+            Console.WriteLine($"{date} { monthOfTheYear} { someVar}");
+        }
+    }
+}
