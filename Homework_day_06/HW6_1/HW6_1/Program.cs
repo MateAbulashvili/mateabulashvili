5ca41edeabd24b2bf559a02542caa929d4efb613
MateAbulashvili <abulashvilimate@gmail.com>
2021-11-03 10:51:18 -0400

    add homework_day_06

---
+++ Homework_day_06/HW6_1/HW6_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HW6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int i, len, vowel, cons;
+
+            
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();
+
+            vowel = 0;
+            cons = 0;
+            len = str.Length;
+
+            for (i = 0; i < len; i++)
+            {
+
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+                {
+                    vowel++;
+                }
+                else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+                {
+                    cons++;
+                }
+            }
+            Console.Write("\n vowel count : {0}\n", vowel);
+            Console.Write("\n consonant count : {0}\n\n", cons);
+        }
+    }
+}
+   
+
---
+++ Homework_day_06/HW6_2/HW6_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HW6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int l = 0;
+
+            
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();
+
+            l = str.Length - 1;
+            Console.Write("The characters of the string in reverse are : \n");
+            while (l >= 0)
+            {
+                Console.Write("{0} ", str[l]);
+                l--;
+            }
+            
+        }
+    }
+}
