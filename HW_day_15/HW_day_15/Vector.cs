0d86caa70284b672539ed4320e706ae303bf155a
MateAbulashvili <92993776+MateAbulashvili@users.noreply.github.com>
2021-11-28 16:38:56 +0400

    Add files via upload

---
+++ HW_day_15/HW_day_15/Vector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_1
+{
+    class Vector
+    {
+        public Vector()
+        {
+
+        }
+        public Vector(int x, int y)
+        {
+            Abscissa = x;
+            Ordinate = y;
+        }
+        public int Abscissa { get; set; }
+        public int Ordinate { get; set; }
+    }
+}
---
+++ HW_day_15/HW_day_15/matrix.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_1
+{
+    class Matrix
+    {
+
+        public int[,] mymatrix { get; set; } = { { 1, 1 }, { 1, 1 } };
+    }
+}
