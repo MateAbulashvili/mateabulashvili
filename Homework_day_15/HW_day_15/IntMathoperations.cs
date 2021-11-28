28807c03e36d16779387a95b8aec7bc0d9f0bc27
MateAbulashvili <abulashvilimate@gmail.com>
2021-11-28 12:39:18 -0500

    add Homework_day_15

---
+++ Homework_day_15/HW_day_15/IntMathoperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_1
+{
+    class IntMathOperation : IMathOperation<int>
+    {
+
+        public int Multiplay(int v1, int v2)
+        {
+            return v1 * v2;
+        }
+
+        public int Subtract(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+
+        public int Sum(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+    }
+}
