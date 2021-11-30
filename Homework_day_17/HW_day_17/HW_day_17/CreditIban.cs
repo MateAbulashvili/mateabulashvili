ddb7066669d7759ba8b77d29c157fe87b9489009
MateAbulashvili <abulashvilimate@gmail.com>
2021-11-30 11:42:22 -0500

    add Homework_day_17

---
+++ Homework_day_17/HW_day_17/HW_day_17/CreditIban.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_day_17
+{
+    public class CreditIban : IBAN
+    {
+        private double _limite = 500;
+        public double CreditBalance { get; private set; }
+        public CreditIban(double value)
+        {
+            CreditBalance = value;
+        }
+        public void GetMoney(double value)
+        {
+            if (CreditBalance - value + _limite < 0)
+            {
+                throw new ExceedLimitException("You Exceed the Limit");
+            }
+            else
+            {
+                CreditBalance = CreditBalance - value;
+            }
+
+        }
+        public void Deposit(double value)
+        {
+            CreditBalance = CreditBalance + value;
+        }
+    }
+}
---
+++ Homework_day_17/HW_day_17/HW_day_17/ExMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_day_17
+{
+    public class ExMessage : Exception
+    {
+        public void GetLastInnerExMessage(Exception ex)
+        {
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+            Console.WriteLine(ex);
+        }
+        public void GetAllInnerExMessageTogether(Exception ex)
+        {
+            Console.WriteLine(ex);
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+    }
+}
